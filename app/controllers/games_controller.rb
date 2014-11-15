eff8be15795fa9aba7f8b12dbb3f4d81b1250b4e
Jordan VanderZwaag <Jordan@clm-jvanderzwaag.attlocal.net>
2014-11-15 03:22:51 -0600

    converted games controller to inherit from base controler

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,66 +1,39 @@
-class GamesController < ApplicationController
+class GamesController < BaseController
   require 'hash_uniq_game_id'
-  before_action :set_game, only: [:show, :edit, :update, :destroy]
-  before_filter :set_default_response_format
 
-  # GET /games
-  # GET /games.json
-
-  def index
-    @games = Game.all
-    @averages = Average.first
-    @teams = Team.all
-  end
-
-  # GET /games/1
-  # GET /games/1.json
-  def show
-    @averages = Average.first
-    @teams = @game.team_ids.map { |team_id| Team.find(team_id) }
-    #    respond_to do |format|
- #     format.json { render json: @game, meta: { averages: @averages} }
-  #  end
-  end
-
-  # PATCH/PUT /games/1
-  # PATCH/PUT /games/1.json
   def update
-    respond_to do |format|
-      if @game.update(game_params)
-        if  @game.winner_id == game_params["winner_id"]
-          format.json { render json: @game, status: :ok, location: @game }
-        else
-          update_records(@game, game_params)
-          format.json { render json: @game, status: :created, location: @game }
-        end
-      else
-        format.json { render json: @game.errors, status: :unprocessable_entity }
-      end
+    if get_resource.update(resource_params)
+      update_records(resource_params) unless get_resource.winner_id == resource_params["winner_id"]
+      set_additional_show_resources
+      render :show
+    else
+      render json: get_resource.errors, status: :unprocessable_entity
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_game
-      @game = Game.find(params[:id])
-    end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def game_params
+    def resource_params
       params.require(:game).permit(:id, :uniq_game_id, :bracket_id, :bracket_game_id, :teams, :winner_id, :in_bracket)
     end
 
-    def set_default_response_format
-      request.format = :json
+    def set_additional_index_resources
+      @teams = Team.all
+      @averages = Average.first
     end
 
+    def set_additional_show_resources
+      @teams = @game.team_ids.map { |team_id| Team.find(team_id) }
+      @teams.push(Team.find(@game.winner_id))
+    end
 
-    def update_records(game, game_params)
-      next_game_uniq_id = next_game_uniq_id(game.bracket_id,
-                                            game.bracket_game_id)
+    def update_records(game_params)
+      next_game_uniq_id = next_game_uniq_id(@game.bracket_id,
+                                            @game.bracket_game_id)
       next_game = Game.find_by(uniq_game_id: next_game_uniq_id)
       team =  Team.find(game_params["winner_id"])
-      updated_team = updated_winning_team(team, game, next_game.id)
+      updated_team = updated_winning_team(team, @game, next_game.id)
       update_next_game(next_game, updated_team)
     end
 
