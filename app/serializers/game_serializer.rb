b1f7a75cc8415ef08d5cdb014fc6660a8d56be2e
Jordan VanderZwaag <Jordan@clm-jvanderzwaag.attlocal.net>
2014-11-14 21:53:24 -0600

    moved deprecated embed to a global config

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,5 +1,4 @@
 class GameSerializer < ActiveModel::Serializer
-  embed :ids, include: true
   attributes :id, :uniq_game_id, :bracket_id, :bracket_game_id, :winner_id, :in_bracket
   has_many :teams
 end
